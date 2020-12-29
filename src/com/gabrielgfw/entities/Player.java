4cd195bf8804c1d08f2034e3b5e1c23798190af7
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-12-29 10:36:40 -0300

    add player class

---
+++ src/com/gabrielgfw/entities/Player.java
@@ -0,0 +1,10 @@
+package com.gabrielgfw.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Player extends Entity {
+
+    public Player(int x, int y, int width, int height, BufferedImage sprite) {
+        super(x, y, width, height, sprite);
+    }
+}
